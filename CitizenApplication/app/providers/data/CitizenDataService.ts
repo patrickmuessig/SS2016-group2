cdf36c6e62b0576f0dcd5c1d7e1017c1e580b226
Patrick Müssig <p_muessig13@cs.uni-kl.de>
2016-05-08 14:05:10 +0200

    #11 Implement RestApiProvider

--- CitizenApplication/app/providers/data/CitizenDataService.ts
+++ CitizenApplication/app/providers/data/CitizenDataService.ts
@@ -26,9 +26,9 @@ export class CitizenDataService implements CitizenDataServiceInterface {
 	private storageApi: PersistentDataProvider;/*TODO initialize*/
 	private cache: CitizenDataCache = new CitizenDataCache;
 	
-	constructor(restApi: RestApiProvider, storageApi: PersistentDataProvider) {
+	constructor(restApi: RestApiProvider, storageApi: PersistentDataProvider, http: Http) {
 		if (!restApi) {
-			this.restApi = new RestApiProvider();
+			this.restApi = new RestApiProvider(http);
 		} else {
 			this.restApi = restApi;
 		}
--- CitizenApplication/app/providers/data/RestApiProvider.ts
+++ CitizenApplication/app/providers/data/RestApiProvider.ts
@@ -8,25 +8,131 @@ import {Line} from '../model/Line';
 import {Route} from '../model/Route';
 import {Stop} from '../model/Stop';
 import {Point} from '../model/geojson/Point';
+import {Injectable, Inject, Component} from 'angular2/core';
+import {Http, Response, URLSearchParams, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/add/operator/map'
 
+@Injectable()
 export class RestApiProvider implements RestApiProviderInterface {
-    constructor() { };
+    private BASE_URL = "http://localhost:3000" // Change to productive adress
+    data: any = null;
+    constructor(@Inject(Http) private http : Http) { };
     getUpdateDataFromServer():UpdateData {
-        return new UpdateData();
+        let data = new UpdateData();
+        let result = this.getJSONString('update', 'busses, lines, routes, stops');
+        
+        result.catch(resultData => {
+           data.busses = resultData.busses;
+           data.lines = resultData.lines;
+           data.routes = resultData.routes;
+           data.stops = resultData.stops; 
+        });
+        
+        return data;
     }
     getBussesFromServer(){
-        return new Array<Bus>();
+        let busses = new Array<Bus>();
+        
+        
+        let result = this.getJSONString('busses', 'busses');
+        
+        result.catch(resultData => {
+            resultData.busses.forEach(busData => {
+            let bus = new Bus();
+            bus.color = busData.color;
+            bus.id = busData.id;
+            bus.numberPlate = busData.numberPlate;
+            bus.pictureLink = busData.picture;
+            busses.push(bus);    
+            }) 
+        });
+        
+        return busses;
     }
     getLinesFromServer(){
-        return new Array<Line>();
+        let lines = new Array<Line>();
+        
+        let result = this.getJSONString('lines', 'lines');
+        
+        result.catch(resultData => {
+            resultData.lines.forEach(lineData => {
+                let line = new Line();
+                line.id = lineData.id;
+                line.busses = lineData.busses;
+                line.name = lineData.name;    
+                line.routeRef = lineData.routeRef;            
+                lines.push(line);
+            });
+        });            
+        
+        return lines;
     }
     getStopsFromServer(){
-        return new Array<Stop>();
+        let stops = new Array<Stop>();
+        let result = this.getJSONString('stops', 'stops, timestamp');
+        result.catch(resultData => {
+            resultData.stops.forEach(stopData => {
+                let stop = new Stop();
+                stop.id = stopData.id;
+                stop.location = new Point(stopData.location.latitude, stopData.location.longitude);
+                stop.name = stopData.name;
+                stop.schedule = stopData.schedule;
+                
+                stops.push(stop);
+            });
+        });            
+        
+        return stops;
+        
     }
     getRoutesFromServer(){
-        return new Array<Route>();
+        let routes = new Array<Route>();
+        
+        let result = this.getJSONString('routes', 'routes');
+        
+        result.catch(resultData => {
+            resultData.routes.forEach(routeData => {
+                let route = new Route();
+                route.id = routeData.id;
+                let gpsPoints = new Array<Point>();
+                routeData.gpsData.forEach(gpsDatas => {
+                    let point = new Point(gpsDatas.latitude, gpsDatas.longitude)
+                    gpsPoints.push(point);
+                })
+                route.gpsData = gpsPoints;
+                routes.push(route);
+            });
+        });            
+        
+        return routes;
     }
     getRealTimeBusData(id: number){
+        let result = this.getJSONString("busses/" + id, "position, delay")
+        let point;
+        let delay = 0;
+        
+        result.catch(resultData => {
+           point = new Point(resultData.position.latitude, resultData.position.longitude);
+           delay = resultData.delay;
+        });
+        
         return { position: new Point(0,0), delay: 0 };
     }
+    
+    private getJSONString(searchString: string, fields: string){
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('results', '0:50');
+        params.set('fields', fields);
+        
+        let url = this.BASE_URL + searchString;
+        
+        return new Promise(resolve => {
+            this.http.get(url, params)
+            .map(res => res.json())
+            .subscribe(data => {
+                this.data = data;
+                resolve(this.data);
+            })
+        })
+     }
 }
